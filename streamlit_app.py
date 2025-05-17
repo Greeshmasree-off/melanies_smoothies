56c1389314faa59b4f00e7cc9cc3da97a4e90176
Greeshmasree-off <gyetukuri2023@fau.edu>
2025-05-17 01:07:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,8 @@ if st.button('Submit Order'):
 
         for fruit_chosen in ingredients_list:
             ingredients_string+=fruit_chosen+' '
+            smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+            sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
 
         #st.write(ingredients_string)
 
@@ -41,6 +43,4 @@ if st.button('Submit Order'):
     else:
         st.warning("Please select atleast one Ingredient")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
+
