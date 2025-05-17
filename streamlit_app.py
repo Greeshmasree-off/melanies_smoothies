b64d146fc4cb4b3bb5f1b53d90ffa312ef6902ad
Greeshmasree-off <gyetukuri2023@fau.edu>
2025-05-17 02:09:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,7 @@ if st.button('Submit Order'):
             search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
             st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
             st.subheader(fruit_chosen + 'Nutrition Information')
-            encoded_fruit = urllib.parse.quote(fruit_chosen)
-            smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + encoded_fruit)
+            smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
             sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
 
         #st.write(ingredients_string)
